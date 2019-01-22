9cff458bfc60e83f5d268f38b382ea6a0c29a31c
Chuck Gilbert <chuck.gilbert@oracle.com>
2019-01-22 12:48:13 -0800

    update datasources

--- datasource.tf
+++ datasource.tf
@@ -54,19 +54,14 @@ data "oci_database_db_home_patch_history_entries" "patches_history" {
   db_home_id = "${data.oci_database_db_homes.db_homes.db_homes.0.db_home_id}"
 }
 
-data "oci_database_db_systems" "db_systems" {
-  compartment_id = "${var.compartment_ocid}"
-  backup_id      = "${oci_database_backup.test_backup.id}"
-}
-
 data "oci_database_db_versions" "test_db_versions_by_db_system_id" {
   compartment_id = "${var.compartment_ocid}"
   db_system_id   = "${oci_database_db_system.domain_db_system.id}"
 }
 
 data "oci_database_db_system_shapes" "domain_db_system_shapes" {
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.availability_domain - 1],"name")}"
-  compartment_id      = "${var.compartment_ocid}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0], "name")"
+  compartment_id      = "${var.db_system["shape"]}"
 
   filter {
     name   = "shape"
