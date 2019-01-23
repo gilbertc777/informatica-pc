a3a607731c0d400a2dfc00de85884774aa20fb9c
Chuck Gilbert <chuck.gilbert@oracle.com>
2019-01-23 08:00:26 -0800

    upodate

--- compute.tf
+++ compute.tf
@@ -9,6 +9,7 @@ resource "oci_database_db_system" "domain_db_system" {
             admin_password = "${var.db_home_database_admin_password}"
             db_name = "${var.db_home_database_db_name}"
             pdb_name = "${var.db_home_database_pdb_name}"
+            db_workload = "${var.db_home_database_db_workload}"
         }
         db_version = "${var.db_home_db_version}"
         display_name = "${var.db_home_display_name}"
@@ -18,8 +19,6 @@ resource "oci_database_db_system" "domain_db_system" {
     shape = "${var.db_shape}"
     ssh_public_keys = ["${tls_private_key.key.public_key_openssh}"]
     subnet_id = "${oci_core_subnet.subnet.id}"
-    db_workload = "${var.db_home_database_db_workload}"
-
     data_storage_size_in_gb = "${var.db_data_storage_size_in_gb}"
     license_model = "${var.db_license_model}"
     node_count = "${var.db_node_count}"
