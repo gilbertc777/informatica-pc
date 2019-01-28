67173e67eb921d64fa071230f0641fd83d18ac0a
Chuck Gilbert <chuck.gilbert@oracle.com>
2019-01-28 06:23:52 -0800

    refactor of fs mount script.

--- scripts/mount-fss.sh
+++ scripts/mount-fss.sh
@@ -13,22 +13,36 @@ echo mount_target_ip $mount_target_ip
 echo export_name $export_name
 echo mount_point $mount_point
 
-if [ ! -z "${mount_target_ip}" && ! -z "${export_name}" && ! -z "${mount_point}" ]
+# Check required variables suppilied
+if [ -z "${mount_target_ip}" ]
 then
-
-    # Install nfs-utils
-    sudo yum -y install nfs-utils > nfs-utils-install.log
-    
-    # create mount point
-    sudo mkdir -p ${mount_point}
-    
-    # Perform one time mount of filesystem
-    sudo mount ${mount_target_ip}:${export_name} ${mount_point}
-
-    # Setup permanent mount params in /etc/fstab
-    echo "${mount_target_ip}:${export_name} ${mount_point}  nfs defaults,relatime 0 0" >> /etc/fstab
-else
-    echo "Not mounting filesystems.  Environment not set!"
+  echo "Variable: mount_target_ip not set!  Exiting."
+  exit 1
 fi
+
+if [ -z "${export_name}" ]
+then
+  echo "Variable: export_name not set!  Exiting."
+  exit 1
+fi
+
+if [ -z "${mount_point}" ]
+then
+    echo "Variable mount_point not set!  Exiting."
+    exit 1
+fi
+
+# With all variables set, mount filesystem
+# Install nfs-utils
+sudo yum -y install nfs-utils > nfs-utils-install.log
+    
+# create mount point
+sudo mkdir -p ${mount_point}
+    
+# Perform one time mount of filesystem
+sudo mount ${mount_target_ip}:${export_name} ${mount_point}
+
+# Setup permanent mount params in /etc/fstab
+echo "${mount_target_ip}:${export_name} ${mount_point}  nfs defaults,relatime 0 0" >> /etc/fstab
 
 # end of script
