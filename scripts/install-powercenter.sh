1126a3a8b70de5566cea25e8e25f2a094473abcb
Chuck Gilbert <chuck.gilbert@oracle.com>
2019-01-29 15:59:50 -0800

    enable installer.

--- scripts/install-powercenter.sh
+++ scripts/install-powercenter.sh
@@ -80,8 +80,6 @@ sed -i s/^DB_CUSTOM_STRING_SELECTION=.*/DB_CUSTOM_STRING_SELECTION=1/ /home/opc/
 sed -i s/^DB_CUSTOM_STRING=.*/DB_CUSTOM_STRING=$DB_CUSTOM_STRING/ /home/opc/infainstaller/SilentInput.properties
 echo "Starting Installation..." >> /home/opc/user_data.log
 ln -s /usr/lib/oracle/18.3/client64/lib/libclntsh.so.18.1 /usr/lib/oracle/18.3/client64/lib/libclntsh.so.11.1
-echo "STOPPING HERE FOR FIRST TEST"
-exit 0
 sh /home/opc/InfaEc2Scripts/linInfaInstallerEc2.sh $admin_console_password $master_db_password PCTest $infra_passphrase
 echo "Installation Complete. Please see installation log for more details." >> /home/opc/user_data.log
 
